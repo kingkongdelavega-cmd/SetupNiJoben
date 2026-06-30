1a930029de786ecaf0afeda33bb06134d70f24b1
itsmicajoycebiadoy <biadoymicajoyce@gmail.com>
2026-06-30 11:31:28 +0800

    Editing OBW2D1-First Test

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import './styles.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from '../Inventory/src/App';
+import '../Inventory/src/styles.css';
 
-const container = document.getElementById('root')
-const root = createRoot(container)
-root.render(<App />)
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
+
